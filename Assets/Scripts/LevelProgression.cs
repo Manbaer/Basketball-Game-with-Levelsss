a92308cc3b441c6389ff6a929619772434ccb1ab
Jonathan Baer <randombossbro@gmail.com>
2020-07-09 22:05:31 -0400

    Initial Unity Commit
    Rawr oops

---
+++ Assets/Scripts/LevelProgression.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelProgression : MonoBehaviour
+{
+    [SerializeField]
+    string strTag;
+    static int counter = 1; 
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        counter++;
+
+        if (counter == 2 && collision.collider.tag == strTag)
+        {
+            SceneManager.LoadScene("Level 2");
+        } else if (counter == 3 && collision.collider.tag == strTag)
+        {
+            SceneManager.LoadScene("Level 3");
+        } else if (counter == 4 && collision.collider.tag == strTag)
+        {
+            SceneManager.LoadScene("Level 4");
+        } else if (counter > 4 && collision.collider.tag == strTag)
+        {
+            SceneManager.LoadScene("End");
+        }
+
+    }
+}
---
+++ Assets/Scripts/RestartLvl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLvl : MonoBehaviour
+{
+    [SerializeField]
+    string strTag;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == strTag)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
+
